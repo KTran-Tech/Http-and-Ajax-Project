c410a69d3f46f28054b81b9aebd363fb3fd40162
ktran-tech <kevinmt191919@gmail.com>
2020-04-26 17:01:41 -0500

    Path through Link

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 // axios is necessary to work with promises(usually an Object with LOTS of data)
 // import axios from 'axios';
+// import axios from '../../axios'
 
-import axios from '../../axios'
+import {Route, Link} from 'react-router-dom'
 
 import './Blog.css';
 
-import Posts from './Posts/Posts'
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost'
+
 
 
 class Blog extends Component {
@@ -22,12 +25,17 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+{/* link is essential because it prevents the page from losing the state and reloading a new page */}
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/new-post">New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-            <Posts />
+
+            <Route path="/" exact component={Posts}/>
+            <Route path="/new-post" exact component={NewPost}/>
+
+
             </div>
         );
     }
